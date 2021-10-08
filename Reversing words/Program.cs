ef60b7b403d14b856fbe713cbd91cb649ae2a9b6
delmar <delmarpre@gmail.com>
2021-10-08 08:53:59 -0400

    finished

---
+++ Reversing words/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace StringReverseUsingStack
+{
+    public static class StringReverse
+    {
+        // Reverse ExtentionMethod  
+        public static string Reverse(this string str)
+        {
+            // calling StringReverseUsingStack  
+            return Program.StringReverseUsingStack(str);
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            
+            Console.WriteLine("Please enter the word you would like to reverse:");
+            string sampleText = Console.ReadLine();
+            if (string.IsNullOrEmpty(sampleText))
+                sampleText = "CSharpcorner";
+            
+            
+            var reverseText = StringReverseUsingStack(sampleText);
+            Console.WriteLine($"Your Word or words reversed is : {reverseText}");
+            
+        }
+        public static string StringReverseUsingStack(string str)
+        {
+            var reversedStr = "";
+            Stack myStack = new Stack();
+            foreach (var t in str)
+                myStack.Push(t);
+            while (myStack.Count > 0)
+                reversedStr += myStack.Pop();
+            return reversedStr;
+        }
+    }
+}
